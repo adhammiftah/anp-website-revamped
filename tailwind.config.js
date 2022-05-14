9db4fc4e4d0bc276441fad0cc8b589b071409d7e
Adham Miftah <77636588+adhammiftah@users.noreply.github.com>
2022-05-14 10:08:59 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'montserrat' : ['montserrat', 'sans-serif'],
+      },
+      backgroundImage: {
+        'hero': "url('../images/home-background.png')",
+      },
+    },
+  },
+  plugins: [],
+}
